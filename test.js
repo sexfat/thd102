edb5fb0ec70bc7884ea5ae7fa5788ed2c9fec351
sexfat <bryant6831@gmail.com>
2023-08-11 11:10:17 +0800

    add

--- test.js
+++ test.js
@@ -5,13 +5,18 @@ const x = 'webpack start';
 console.log(x);
 
 //  被景色
-$('body').css('background-color' , '#202020');
+$('body').css('background-color', '#202020');
 
 
 //動畫
-gsap.to('.box' , {
- x : 400,
- y : 500,
- duration: 3,
- rotation: 180
+gsap.to('.box', {
+    x: 400,
+    y: 500,
+    duration: 3,
+    rotation: 180,
+    repeat: -1,
+    yoyo: true,
+    //opacity: 0,
+    scale: 5,
+    ease: "elastic.out(1, 0.3)"
 })
