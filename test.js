2a834bcfe26ad7a246942e440a3c7741bdf5bca3
sexfat <bryant6831@gmail.com>
2023-08-11 10:01:35 +0800

    add gsap

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-import $ from 'jquery' ;
+import $ from 'jquery';
+import { gsap } from "gsap";
 
 const x = 'webpack start';
 console.log(x);
 
 //  被景色
-$('body').css('background-color' , 'red');
+$('body').css('background-color' , '#202020');
+
+//動畫
